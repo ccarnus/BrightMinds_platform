fc242454256aa94cf7c6e75017aed7997bb164a9
Clement CARNUS <clement.carnus@exotec.com>
2025-11-15 22:38:33 -0500

    changing the server.js config to http insteead of https

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
-const https = require('https');
+const https = require('http');
 const fs = require('fs');
 const app = require('./app');
 
 // HTTPS configuration
-const PORT = process.env.PORT || 443;
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/api.brightmindsresearch.com/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/api.brightmindsresearch.com/fullchain.pem'),
-};
+const PORT = process.env.PORT || 3000;
+const httpServer = http.createServer(app);
 
-// Create HTTPS server
-const httpsServer = https.createServer(options, app);
-
-httpsServer.listen(PORT, () => {
-  console.log(`Server is running on https://localhost:${PORT}`);
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
